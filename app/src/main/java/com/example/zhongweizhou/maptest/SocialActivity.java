1fb47165ca84387ff8f589a53f75b132621d9bb3
jezhou8 <jezhou8@students.d125.org>
2017-03-14 16:12:57 -0500

    something

--- app/src/main/java/com/example/zhongweizhou/maptest/SocialActivity.java
+++ app/src/main/java/com/example/zhongweizhou/maptest/SocialActivity.java
@@ -1,21 +1,13 @@
 package com.example.zhongweizhou.maptest;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.facebook.AccessToken;
-import com.facebook.GraphRequest;
-import com.facebook.GraphResponse;
-import com.facebook.HttpMethod;
 import com.facebook.Profile;
 import com.facebook.login.widget.ProfilePictureView;
 
-import java.net.URL;
 
 /**
  * Created by zhongweizhou on 3/11/17.
@@ -34,22 +26,7 @@ public class SocialActivity extends AppCompatActivity{
 
     }
 
-    public void printId(View view){
+    public void printId(View view) {
         Toast.makeText(getApplicationContext(), Profile.getCurrentProfile().getId(), Toast.LENGTH_SHORT).show();
     }
-
-    public Bitmap getFacebookProfilePicture(String userID){
-        try {
-            URL imageURL = new URL("https://graph.facebook.com/" + userID + "/picture?type=large");
-            Bitmap bitmap = BitmapFactory.decodeStream(imageURL.openConnection().getInputStream());
-            return bitmap;
-
-        }catch(Exception e){
-            Toast.makeText(getApplicationContext(), "unable to get image", Toast.LENGTH_SHORT);
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-
 }
